f196f1d61176f5ca222d904556a04d6f1355a956
Sirjna <bhandarisirjana85@gmail.com>
2024-08-23 21:20:06 +0545

    Update BookStore.java

--- BookStore.java
+++ BookStore.java
@@ -4,110 +4,62 @@ import java.util.Scanner;
 
 public class BookStore {
 
-    @SuppressWarnings({ "unused", "resource" })
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-        //Define book properties
+        Scanner scanner = new Scanner(System.in);
 
-       
-		String title;
-		String author;
-         String  price;
-         String publisher;
-         String ISBN;
+        System.out.print("How many books do you want to add: ");
+        int size = scanner.nextInt();
+        scanner.nextLine();  // Consume the newline character
 
+        String[] books = new String[size];
 
-        //scan size of book
-         Scanner scanner=new Scanner(System.in);
-         System.out.println("How many books do you want to Add: ");
-         int size=scanner.nextInt();
-         String[] books=new String[size];  //2 book size index[0]=1 book, index[1]=2b book
+        // Reading book details from user
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter details for book " + (i + 1));
+            System.out.print("Title: ");
+            String title = scanner.nextLine();
+            System.out.print("Author: ");
+            String author = scanner.nextLine();
+            System.out.print("Price: ");
+            String price = scanner.nextLine();
+            System.out.print("Publisher: ");
+            String publisher = scanner.nextLine();
+            System.out.print("ISBN: ");
+            String isbn = scanner.nextLine();
 
-
-         //read book properties from user or keyboard
-         for(int i=0;i<books.length;i++){
-             System.out.println("Enter Book Title: ");
-            String bookTitle=scanner.next();
-
-             System.out.println("Enter Book Author: ");
-            String bookAuthor=scanner.next();
-
-             System.out.println("Enter Book Price: ");
-             String bookPrice=scanner.next();
-
-             System.out.println("Enter Book Publisher: ");
-             String bookPublisher =scanner.next();
-
-             System.out.println("Enter Book ISBN: ");
-            String isbn=scanner.next();
-
-            // bundling all properties
-           String bookInfo=bookTitle+","+bookAuthor+","+bookPrice+","+bookPublisher+","+isbn;
-
-
-            books[i]=bookInfo;
-
-         }
-
-         //Display list of book
-
-       System.out.print("    "+"Title"+"      ");
-        System.out.print("    "+"Author"+"    ");
-        System.out.print("    "+"Price"+"    ");
-        System.out.print("    "+"Publisher"+"    ");
-        System.out.print("  "+"ISBN"+"    \n");
-        System.out.print("   "+"========"+"  ");
-        System.out.print("   "+"========"+"  ");
-        System.out.print("   "+"========"+"  ");
-        System.out.print("   "+"========"+"  ");
-        System.out.print("      "+"========"+"    \n");
-
-
-        // Get all book list
-
-         for(String book:books){  //(bookTitle,bookAuthor,bookPrice,bookPublisher,isbn)
-             String[] singleBook=book.split(",");
-             System.out.print("    "+singleBook[0]+"    ");
-             System.out.print("    "+singleBook[1]+"    ");
-             System.out.print("    "+"£"+singleBook[2]+"    ");
-             System.out.print("    "+singleBook[3]+"    ");
-             System.out.print("       "+singleBook[4]+"    \n");
-         }
-
-
-         // Calculate books Stock
-
-        double totalCost=200;//300+200+----- //500
-        double maximum=0;
-        double minimum=0; //300
-
-        for(String book:books){
-            String[] singleBook=book.split(",");
-            double bookPrice=Double.parseDouble(singleBook[2]);
-
-            totalCost+=bookPrice; // totalCost=totalCost+bookPrice; 300,200
-
-            if(bookPrice>maximum){  //300>0, 400>300
-                maximum=bookPrice;  //maximum=400
-            }
-
-            minimum=bookPrice;  //300 ,400
-            if(bookPrice<minimum){// 200<300
-                minimum=bookPrice;
-            }
-
+            books[i] = String.join(",", title, author, price, publisher, isbn);
         }
 
-        //Display Book Stock
+        // Displaying the book list
+        System.out.println("\nTitle\tAuthor\tPrice\tPublisher\tISBN");
+        for (String book : books) {
+            String[] details = book.split(",");
+            System.out.println(details[0] + "\t" + details[1] + "\t£" + details[2] + "\t" + details[3] + "\t" + details[4]);
+        }
 
-        System.out.println("========Totals Stocks===========");
+        // Calculating book stock details
+        double totalCost = 0;
+        double maxPrice = Double.MIN_VALUE;
+        double minPrice = Double.MAX_VALUE;
 
-        System.out.println("Total number of books: "+books.length);
-        System.out.println("Total cost of books: "+"£"+totalCost);
-        System.out.println("Maximum cost of a book: "+"£"+maximum);
-        System.out.println("Minimum cost of book: "+"£"+minimum);
-        System.out.println("Average cost of book: "+"£"+totalCost/books.length);
+        for (String book : books) {
+            String[] details = book.split(",");
+            double price = Double.parseDouble(details[2]);
 
+            totalCost += price;
+            if (price > maxPrice) maxPrice = price;
+            if (price < minPrice) minPrice = price;
+        }
 
+        // Displaying stock details
+        System.out.println("\n========Totals Stocks===========");
+        System.out.println("Total number of books: " + size);
+        System.out.println("Total cost of books: £" + totalCost);
+        System.out.println("Maximum cost of a book: £" + maxPrice);
+        System.out.println("Minimum cost of book: £" + minPrice);
+        System.out.println("Average cost of book: £" + (totalCost / size));
+
+        scanner.close();
     }
 }
